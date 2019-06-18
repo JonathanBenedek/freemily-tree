7fd80cd34864cbfb528103385f1941db2f62854a
Jonathan Benedek <jonathan.b@tlv.mce-sys.com>
2019-06-18 16:47:19 +0300

    README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # freemily-tree
+
+prerequisites:
+1. Python installed
+
+install:
+1. Clone the project
+
+run:
+1. Open command line from the root directory.
+2. Write the 'run' and click enter, local host should be open (python 3.7.0).
+3. If your python version doesnt supported at the command above, please run manually server.
+4. From your chrome get to the http://localhost:8000 ( 8000 = port your server is  listener).
+
--- authGoogle.js
+++ authGoogle.js
@@ -2,9 +2,6 @@
 	//			https://docs.google.com/spreadsheets/d/13D2fRpETQ4EIMDAI0SMNV4QmfNo4pw6hG2s-PYfLY-Y/edit?usp=sharing
 
 	// Client ID and API key from the Developer Console
-	var CLIENT_ID = '982273298045-a4e2v1fptef7aiqkmsn1b8m5tthhi3lr.apps.googleusercontent.com';
-	var API_KEY = 'AIzaSyDmswC31PZz-GcwXgz6VVWVA-PM5rUK9-A';
-	var SHEET_ID = '13D2fRpETQ4EIMDAI0SMNV4QmfNo4pw6hG2s-PYfLY-Y';
 	// Array of API discovery doc URLs for APIs used by the quickstart
 	var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
 
